116d41c71c6822534726295cb44bd92121b89525
janani-ks <123439325+janani-ks@users.noreply.github.com>
2023-03-03 14:44:30 +0530

    Update Bank.java

--- TraineeProjects/src/main/java/allprojects/Bank.java
+++ TraineeProjects/src/main/java/allprojects/Bank.java
@@ -9,52 +9,57 @@ public class Bank {
 	static PrintStream display=new PrintStream((new FileOutputStream(FileDescriptor.out)));
 	static long accountnumber;
 	static double amount = 0;
-	static boolean storebalance() {
-		Scanner scan = new Scanner(System.in);
-		try {
-		display.print("Balace Amount : ");
-    	amount = scan.nextDouble();
-    	return false;
-		}
-		catch(InputMismatchException e) {
-	    return true;
-		}
+	static double depositeamount;
+	static double withdrawamount;
+	private Bank() {
+		
 	}
-	static boolean storenumber() {
+	static void storebalance() {
+		do {
 		Scanner scan = new Scanner(System.in);
 		try {
-		display.print("Account Number : ");
-    	accountnumber = scan.nextLong();
-    	return false;
+    	amount = scan.nextDouble();
+    	break;
 		}
 		catch(InputMismatchException e) {
-	    return true;
+			display.print("Enter Valid Amount : ");
+		}}while(1>0);
+	}
+	static void storenumber() {
+		do {
+		Scanner scan = new Scanner(System.in);
+		try {
+    	accountnumber = scan.nextLong();
+    	break;
 		}
+		catch(InputMismatchException e) {
+			display.print("Account Number : ");
+		}}while(1>0);
 	}
 	public static void methodCall() throws InsufficientFundsException{
     	Scanner scan = new Scanner(System.in);
     	Scanner scan1 = new Scanner(System.in);
     	String holdername;
     	int choice;
-    	double depositeamount;
-    	double withdrawamount;
     	display.println("Enter the Details to create account :");
-    	while(storenumber());
+    	display.print("Account Number : ");
+    	storenumber();
     	display.print("Account Holder's Name : ");
     	holdername =  scan.nextLine();
-    	while(storebalance());
+    	display.print("Balace Amount : ");
+    	storebalance();
         Account obj = new Account(accountnumber , holdername ,amount);
         display.println("Enter the choice you want :\n 1.Deposite\n 2.Withdraw \n 3.Check Balance Enquiry");
         choice = scan1.nextInt();
         switch(choice) {
         case 1:
         	display.println("Enter the amount to deposite:");
-        	depositeamount = scan1.nextDouble();
+        	storebalance();
         	obj.deposite(depositeamount);
         	break;
         case 2:
         	display.println("Enter the amount to withdraw:");
-        	withdrawamount = scan1.nextDouble();
+        	storebalance();
         	try {
                 obj.withdraw(withdrawamount);
             }
